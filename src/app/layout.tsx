e9d059d1ab1d3af7f20c50d15aac11f566e31599
Toni Suwendi <tonisuwen@gmail.com>
2024-06-06 10:17:24 +0700

    setup project

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import type { Metadata } from 'next';
+import { Lato } from 'next/font/google';
+import { ColorSchemeScript, MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
+import '@styles/globals.css';
+
+const inter = Lato({ subsets: ['latin'], weight: ['400', '700'] });
+
+export const metadata: Metadata = {
+  title: 'Simple Quicks',
+  description: 'Simple Quicks is a collection of simple and quick recipes.'
+};
+
+const RootLayout: React.FC<{ children: React.ReactNode }> = ({
+  children
+}) => (
+  <html lang="en">
+    <head>
+      <ColorSchemeScript />
+      <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+    </head>
+    <body className={inter.className}>
+      <MantineProvider>
+        {children}
+      </MantineProvider>
+    </body>
+  </html>
+);
+
+export default RootLayout;
---
+++ tailwind.config.ts
@@ -0,0 +1,57 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      },
+      colors: {
+        primary: {
+          blue: '#2F80ED',
+          graphite: '#F2F2F2',
+          slate: '#828282',
+          silver: '#E0E0E0'
+        },
+        indicator: {
+          ember: '#F8B76B',
+          royal: '#8785FF',
+          crimson: '#EB5757',
+          dandelion: '#F2C94C'
+        },
+        chats: {
+          peach: {
+            light: '#FCEED3',
+            dark: '#E5A443'
+          },
+          lavender: {
+            light: '#EEDCFF',
+            dark: '#9B51E0'
+          },
+          mint: {
+            light: '#D2F2EA',
+            dark: '#43B78D'
+          }
+        },
+        stikers: {
+          frost: '#E9F3FF',
+          peach: '#FDCFA4',
+          cream: '#F9E9C3',
+          aqua: '#AFEBDB',
+          sage: '#CBF1C2',
+          periwinkle: '#CFCEF9',
+          orchid: '#F9E0FD'
+        }
+      }
+    }
+  },
+  plugins: []
+};
+export default config;
